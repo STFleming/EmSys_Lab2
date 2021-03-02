974af52add2bd1c7e788aed0b053633a9443ee82
Shane Fleming <shane.t.fleming@gmail.com>
2021-03-02 22:49:52 +0000

    Adding Question4 code

---
+++ src/Question4/Question4.ino
@@ -0,0 +1,32 @@
+// Question4
+// author: stf
+
+#define SIZE 25 
+
+// Volatile keyword stops the compiler optimising
+// the arrays away
+volatile float a[SIZE];
+volatile float b[SIZE];
+volatile float c;
+
+void dotProduct() {
+   for(int i=0; i<SIZE; i++) {
+      c += a[i] * b[i];
+   }	
+}
+
+void randomise(){
+   for(int i=0; i<SIZE; i++) {
+      a[i] = esp_random();
+      b[i] = esp_random();
+   }	
+}
+
+void setup() {
+  randomise();
+}
+
+void loop() {
+  randomise();
+  dotProduct();
+}
